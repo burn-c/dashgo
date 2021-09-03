609459377ddfa837de80e437bc6f99942ec7d14a
Carlos Oliveira <chopcsom@gmail.com>
2021-09-02 22:43:14 -0300

    feat(comp): adds Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
-import { Header } from "../components"
+import { Flex } from '@chakra-ui/react'
+import { Header, Sidebar } from "../components"
+
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
