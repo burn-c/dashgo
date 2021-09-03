86481515c3f3d8ebc99e9d8156fdd9c5669abba9
Carlos Oliveira <chopcsom@gmail.com>
2021-09-02 21:25:58 -0300

    feat(comp): adds feats on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -49,6 +49,30 @@ export function Header() {
         <Icon as={RiSearchLine} fontSize="20" />
       </Flex>
 
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          m="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="28" />
+          <Icon as={RiUserAddLine} fontSize="28" />
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Carlos Oliveira</Text>
+            <Text color="gray.300" fontSize="small">carlos.burndev@gmail.com</Text>
+          </Box>
+          <Avatar size="md" name="Carlos Oliveira" src="https://avatars.githubusercontent.com/u/54965836?v=4" />
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
