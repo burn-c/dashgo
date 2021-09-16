acd51369549e82630640b63428127d6f21ad46c4
Carlos Oliveira <chopcsom@gmail.com>
2021-09-15 22:40:21 -0300

    style(comp): adds responsiveness to Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,19 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Carlos Oliveira</Text>
-        <Text color="gray.300" fontSize="small">carlos.burndev@gmail.com</Text>
-      </Box>
+      {showProfileData &&
+        <Box mr="4" textAlign="right">
+          <Text>Carlos Oliveira</Text>
+          <Text color="gray.300" fontSize="small">carlos.burndev@gmail.com</Text>
+        </Box>
+      }
+
       <Avatar size="md" name="Carlos Oliveira" src="https://avatars.githubusercontent.com/u/54965836?v=4" />
     </Flex>
   )
